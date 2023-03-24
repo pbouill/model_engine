71cfc968d8c80dd27f9624dd55bef917e1eb487d
hatch-pbouillon <paul.bouillon@hatch.com>
2023-03-24 16:17:53 -0400

    got predictions and trainign working :)

---
+++ model_engine/model/ultralytics_backend.py
@@ -0,0 +1,301 @@
+from PIL import Image
+import requests
+from io import BytesIO
+import random
+from pathlib import Path
+import yaml
+import json
+import numpy as np
+
+import logging
+import coloredlogs
+
+DEFAULT_FORMAT = ('%(asctime)s.%(msecs)06d :: %(levelname)s :: '
+                  '%(name)s :: %(module)s.%(funcName)s:%(lineno)d - %(message)s')
+
+logging.basicConfig(format=DEFAULT_FORMAT)
+logger = logging.getLogger(__name__)
+coloredlogs.install(fmt=DEFAULT_FORMAT, logger=logger, level='INFO')
+
+#TODO: Patch label_studio_ml\model.py -- no need for key in the _current_model -- not a dict!
+
+from label_studio_ml.model import LabelStudioMLBase
+from label_studio_ml.utils import get_single_tag_keys
+
+from ultralytics import YOLO
+from ultralytics.yolo.engine.results import Boxes, Results
+from ultralytics.yolo.data import YOLODataset
+
+RECT_LABEL_TYPE = 'RectangleLabels'
+IMG_TYPE = 'Image'
+
+LABEL_CONFIG_FILE = '.data/label_config.xml'
+MODEL_FILE = '.data/best.pt'
+
+LS_HOSTNAME = 'http://cacplcemudev.corp.hatchglobal.com:8080'
+
+TRAIN_VAL_TEST_WEIGHT = (85, 15, 0)
+TRAIN_EPOCHS = 2
+
+DATASET_DIR = 'datasets'
+MODEL_DIR = 'models'
+
+class LabelStudioResultValue:
+    def __init__(self, value: dict, cls_dict: dict):
+        self.width = value['width'] / 100
+        self.height = value['height'] / 100
+        self.centre_x = value['x'] / 100 + self.width / 2  # add half the width to get the centre
+        self.centre_y = value['y'] / 100 + self.height / 2  # add half the height to get the centre
+        self.cls = cls_dict[value['rectanglelabels'][0]]
+
+    @property
+    def annotation(self):
+        return f'{self.cls} {self.centre_x} {self.centre_y} {self.width} {self.height}'
+
+
+class YOLOResults:
+    def __init__(self, result: Results, from_name: str, to_name: str, res_type=RECT_LABEL_TYPE):
+        self.results = []
+        self.scores = []
+        self.rotation = 0
+        self.type = 'rectanglelabels' #res_type.lower()
+        self.names = result.names
+        self.original_width, self.original_height = result.orig_shape
+        id = 0
+        for b in result.boxes:
+            yr = YOLOResult(b, self.names)
+            self.scores.append(yr.score)
+            r = yr.result
+            r['id'] = str(id)
+            r['from_name'] = from_name
+            r['to_name'] = to_name
+            r['type'] = res_type.lower()
+            r['image_rotation'] = 0
+            r['original_width'] = self.original_width
+            r['original_height'] = self.original_height
+            self.results.append(r)
+            id += 1
+
+
+    @property
+    def prediction(self):
+        return {
+            # 'model_version': self.model_version,
+            'result': self.results,
+            'score': sum(self.scores) / len(self.scores)
+        }
+
+
+class YOLOResult:
+    def __init__(self, box: Boxes, names: dict):
+        self.rotation = 0
+        self.x, self.y = box.xyxyn[0][0:2].numpy() * 100
+        self.width, self.height = box.xywhn[0][2:4].numpy() * 100
+        self.label = names[int(box.cls)]
+        self.score = float(box.conf * 100)
+    
+    @property
+    def value(self):
+        return {
+            'rotation': float(self.rotation),
+            'x': float(self.x),
+            'y': float(self.y),
+            'width': float(self.width),
+            'height': float(self.height),
+            'rectanglelabels': [self.label]
+        }
+    
+    @property
+    def result(self):
+        return {
+            'score': self.score,
+            'image_rotation': float(0),
+            'value': self.value
+        }
+
+# good example here: https://github.com/heartexlabs/label-studio/discussions/1623#discussioncomment-1507940
+class YOLOModel(LabelStudioMLBase):
+    ACCESS_TOKEN = None
+    def __init__(self, **kwargs):
+        logger.info(f'Creating a new model object with kwargs: {kwargs}')
+        kwargs['hostname'] = LS_HOSTNAME
+
+        # self.model = YOLO(MODEL_FILE)
+        self.model = YOLO('models/yolov8n.pt')
+        self.project = None
+
+        if 'access_token' in kwargs:
+            logger.debug(f'...storing the access token for later use: {kwargs["access_token"]}')
+            self.__class__.ACCESS_TOKEN = kwargs['access_token']  # store the access token class-wide... seems it does not get passed when creating a new instance for training (limits access to images)
+        else:
+            logger.debug(f'...stealing the access token from the class: {self.__class__.ACCESS_TOKEN}')
+            kwargs['access_token'] = self.__class__.ACCESS_TOKEN
+        
+        if 'label_config' not in kwargs:
+            with open(LABEL_CONFIG_FILE, "r") as f:
+                kwargs['label_config'] = f.read()
+        super().__init__(**kwargs)
+        # print(f'MLBase dir: {dir(self)}')
+        self.from_name, self.to_name, self.value, self.labels_in_config = get_single_tag_keys(
+            self.parsed_label_config,
+            RECT_LABEL_TYPE,
+            IMG_TYPE
+        )
+        
+        # if the mdoel has updates from training... load it!
+        if self.train_output:
+            logger.info(f'loading trained model: {self.train_output}')
+            self.model.load(self.train_output['model_path'])
+    
+    def _get_image(self, task):
+        url = task['data'][self.value]
+        logger.debug(f'...getting image: {url}')
+        response = self.api_get(url)
+        img = Image.open(BytesIO(response.content))
+        return img, Path(url)
+    
+    def api_get(self, url):
+        headers = {'Authorization': 'Token ' + self.access_token}
+        logger.debug(f'...requesting data from: {self.hostname + url} with headers {headers}')
+        return requests.get(self.hostname + url, stream=True, headers=headers)
+    
+    def api_get_json(self, url):
+        return json.loads(self.api_get(url).content)
+
+    def predict(self, tasks, model_file=MODEL_FILE, **kwargs):
+        logger.info('##### PREDICT CALL! #####')
+        model = YOLO(model_file)
+        predictions = []
+        logger.info(f'Total number of tasks received: {len(tasks)}')
+        for task in tasks:
+            print(f'Task: {task}')
+            if 'frame' in kwargs:
+                img = kwargs['frame']
+            else:
+                img = self._get_image(task)[0]
+
+            results = model(img)[0]
+            pred = YOLOResults(result=results, from_name=self.from_name, to_name=self.to_name)
+            predictions.append(pred.prediction)
+        return predictions
+
+    def fit(self, tasks, workdir=None, ds_split=TRAIN_VAL_TEST_WEIGHT, epochs=TRAIN_EPOCHS, dataset=None, **kwargs):
+        logger.info('##### TRAINING CALL! #####')
+        logger.info(f'Fit data working directory: {workdir}')
+        task_list = list(tasks)
+
+        if (not len(task_list) > 0) and (dataset is None): 
+            logger.warning('...exiting, no tasks or dataset provided to train on...')
+            return
+        
+        if dataset is None:
+            dataset_path = Path(DATASET_DIR, Path(workdir).name)
+            dataset = self.create_dataset(dataset_path=dataset_path, task_list=task_list, names=[], ds_split=ds_split)
+
+        proj = self.project
+        if proj is None:
+            if len(task_list) > 0:
+                self.get_project(task_list[0])
+        if proj is None:
+            proj = Path(workdir).name
+
+        self.model.train(data=str(dataset), epochs=epochs, project='models/'+proj)
+        train_result = {
+            'checkpoints': self.model.trainer.wdir.as_posix(),
+            'model_path': self.model.trainer.best.as_posix(),
+            'epochs': self.model.trainer.epochs,
+            'batch_size': self.model.trainer.batch_size,
+            'metrics': self.model.trainer.metrics,
+            'data': self.model.trainer.data
+        }
+        logger.info(f'Training completed with the following result: {train_result}')
+        return train_result
+    
+
+    def create_dataset(self, dataset_path: Path, task_list, names = [], ds_split = TRAIN_VAL_TEST_WEIGHT):
+        logger.info(f'creating a new dataset: {dataset_path}')
+
+        train_path = Path(dataset_path, 'train')
+        val_path = Path(dataset_path, 'val')
+        test_path = Path(dataset_path, 'test')
+        for p in [dataset_path, train_path, val_path, test_path]: 
+            if not p.exists():
+                p.mkdir()
+
+        cls_dict = {}
+        # check the names the model is expecting first...
+        for i, n in enumerate(names):
+            cls_dict[n] = i  # assign a cls_id from zero, incrementing upwards
+        # add any missing labels to our class dictionary
+        for l in self.labels_in_config:
+            if l not in cls_dict:
+                cls_dict[l] = len(cls_dict)  # add the label to the dict with next sequential cls_id
+
+        # form the dictionary for the dataset.yml
+        ds_json = {
+            'path': str(dataset_path.resolve()),
+            'train': 'train',
+            'val': 'val',
+            'test': 'test',
+            'names': list(cls_dict.keys())
+        }
+
+        ds_yml_path = Path(dataset_path, 'dataset.yaml')
+        with open(str(ds_yml_path), 'w') as yml:
+            yaml.dump(ds_json, yml)
+
+        num_tasks = len(task_list)
+        split_ratio = np.array(ds_split) / sum(ds_split)
+        num_train = int(num_tasks * split_ratio[0])
+        num_val = int(num_tasks * split_ratio[1])
+        num_test = num_tasks - num_train - num_val
+        logger.info(f'...will use random {num_train}/{num_tasks} for training, {num_val}/{num_tasks} for training validation, and {num_test}/{num_tasks} for final testing')
+        
+        random.shuffle(task_list)  # shuffle this bad boy up to ensure a good mix of images...
+
+        for idx, t in enumerate(task_list):
+            if self.project is None:
+                self.get_project(t)
+
+            logger.debug(t)
+            if idx < num_train:
+                save_path = train_path
+            elif idx < (num_train + num_val):
+                save_path = val_path
+            else:
+                save_path = test_path
+            img, f = self._get_image(t)
+            img.save(str(Path(save_path, f.name)))
+            # file_list.append(f.name)
+            annotations = []
+            
+            if 'annotations' in t:
+                for a in t['annotations']:
+                    if a['was_cancelled']:
+                        print(f'Annotation [{a["id"]}] was cancelled... checking the next')
+                        continue
+                    for r in a['result']:
+                        # print(r)
+                        if 'value' in r:
+                            v = LabelStudioResultValue(r['value'], cls_dict=cls_dict)
+                            annotations.append(v.annotation + '\n')
+                    break  # we already found a valid annotation list, stop looking... will maybe get duplicates(?)
+            with open(str(Path(save_path, f.stem + '.txt')), 'w') as f:
+                f.writelines(annotations)
+        
+        return ds_yml_path  # return the path to the dataset we created...
+    
+
+    def get_project(self, task):
+        if 'project' in task:
+            proj = self.api_get_json(url = f'/api/projects/{task["project"]}/')
+            logger.info(f'Got project info: {proj}')
+            self.project = proj['title']
+            return proj
+        
+    
+if __name__ == '__main__':
+    mod = YOLO(MODEL_FILE)
+    ds = Path('datasets/1679674886/dataset.yaml')
+    mod.train(data=str(ds.absolute()), project='models/1200L', epochs=TRAIN_EPOCHS)
+    
